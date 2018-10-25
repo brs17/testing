71e16bd71647c992d4c1f77491e45f10385e1947
shpurk <benjamin@system76.com>
2018-10-25 08:25:53 -0600

    Added autostart and autologin

--- cpufantest.sh
+++ cpufantest.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+#setup autostart and autologin
+if [ ! -f ~/.config/autostart/cpufantest.desktop ]; then
+    echo '
+    [Desktop Entry]
+    Type=Application
+    Exec=sh -c "gnome-terminal ~/cpufantest.sh"
+    Hidden=false
+    NoDisplay=false
+    X-GNOME-Autostart-enabled=true
+    Name=cpufantest
+    ' >> ~/.config/autostart/cpufantest.desktop
+    sed -i -e 's/#  AutomaticLoginEnable = true/AutomaticLoginEnable = true/g' /etc/gdm3/custom.conf
+    sed -i -e 's/#  AutomaticLogin = user1/AutomaticLogin = $USER/g' /etc/gdm3/custom.conf
 
+fi
+#cat >~/.config/autostart/cpufantest.desktop << EOL
+
+#Run tests
 REBOOT_DURATION=$(( RANDOM%180+180 )) #random reboot between 3-5 minutes
 FAN_SPEED="`sensors | grep "CPU fan" | awk '{print $3 "\t"}'`"
 if [ "$FAN_SPEED" -eq 0 ]
